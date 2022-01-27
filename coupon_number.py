b4ce528d29743e8e880411bfe50889622cb96bee
Priya412-coder <priyankasingh8249@gmail.com>
2022-01-27 08:50:16 +0530

    adding coupon number problem

---
+++ coupon_number.py
@@ -0,0 +1,36 @@
+import random
+
+class CouponNumber():
+
+    def __init__(self, take_number):
+        self.take_number = take_number
+
+    def calculateDistinctNumber(self):
+
+        distict_number = []
+        for x in range(take_number):
+            rand = random.randrange(take_number)
+            if rand not in distict_number:
+                distict_number.append(rand)
+            else:
+                pass
+        return distict_number
+
+
+while True:
+    try:
+        take_number = int(input("Enter the distinct number : "))
+        if take_number < 0:
+            print("Please Enter Positive Digits")
+            continue
+            """
+                taking input from user
+            """
+        coupon_number_object = CouponNumber(take_number)
+        total_random_number = coupon_number_object.calculateDistinctNumber()
+        print(" Distinct Coupon Number : ", total_random_number)
+
+    except ValueError:
+        print("Exception Occured,Please numeric value")
+
+
